b99c8d440cd21d418f786f9eae6b12389d11aeba
Peter Morgan <pete@daffodil.uk.com>
2013-05-22 03:47:13 +0100

    opts

--- conf.py
+++ conf.py
@@ -268,4 +268,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'python': ('http://docs.python.org/3.2', None)}
+intersphinx_mapping = {
+    'xlwt': ('https://xlwt.readthedocs.org/en/latest/', None),
+    'python': ('http://docs.python.org/3.2', None),
+    }
